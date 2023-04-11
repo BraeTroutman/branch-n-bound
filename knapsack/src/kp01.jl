6d6991b4b9b3e08799513a87599c7b073fadbb95
BraeTroutman <braetroutman@gmail.com>
2023-04-11 12:33:56 -0400

    add dp solutions

--- knapsack/src/kp01.jl
+++ knapsack/src/kp01.jl
@@ -1,8 +1,72 @@
 module kp
-export ilpknapsack, dncknapsack, readwv
+export ilpknapsack, dncknapsack, dpknapsack, amkknapsack, readwv
 
 using JuMP
 using HiGHS
+
+function merge(left, right)
+	out = fill((0,0), length(left) + length(right))
+
+    l = 1
+    r = 1
+
+	for i in 1:length(out)
+        if l > length(left)
+			out[i:end] = right[r:end]
+            break
+		end
+        if r > length(right)
+			out[i:end] = left[l:end]
+            break
+		end
+        if left[l] < right[r]
+            out[i] = left[l]
+            l += 1
+        else
+            out[i] = right[r]
+            r += 1
+		end
+	end
+    
+	return out
+end
+
+addtuple((x,y), (a,b)) = (x+a,y+b)
+showrow(row) = join(["<$(w),$(v)>" for (w,v) in row], " ")
+
+function add(row, w, v)
+	[addtuple(tp, (w,v)) for tp ∈ row]
+end
+
+function kill(row, W)
+	(wp, vp) = row[1]
+	out = [(wp, vp)]
+
+	for (w, v) in row[2:end]
+		if w <= W && v >= vp
+			push!(out, (w,v))
+			wp, vp = w, v
+		end
+	end
+
+	return out
+end
+
+function amkknapsack(W, w, v)
+	row = [(0,0)]
+	i::Int32 = 0	
+	W <= 20 && println("$(i): ", showrow(row))
+
+	for i in 1:length(w)
+		next = add(row, w[i], v[i])
+		row = merge(row, next)
+		row = kill(row, W)
+		W <= 20 && println("$(i): ", showrow(row))
+	end
+
+	return row
+end
+
 
 function ilpknapsack(W, w, v)
 	model = Model(HiGHS.Optimizer)
@@ -19,8 +83,8 @@ end
 
 getitems(items) = findall(x -> x > 0.5, items)
 
-function solve(W::Int64, w::Vector{Int64}, v::Vector{Int64})
-    Rows = fill(0, (2, W+1))
+function dpknapsack(W::Int64, w::Vector{Int64}, v::Vector{Int64})
+    Rows = zeros(2, W+1)
 
     for i = 1:length(v)
         for c = 0:W
@@ -55,8 +119,8 @@ function dncknapsack(W::Int64, i, w::Vector{Int64}, v::Vector{Int64})
 	vl = v[1:div(end,2)]
 	vr = v[div(end,2)+1:end]
 
-	x1 = solve(W, wl, vl) # O(nW)
-	x2 = solve(W, wr, vr) # O(nW)
+	x1 = dpknapsack(W, wl, vl) # O(nW)
+	x2 = dpknapsack(W, wr, vr) # O(nW)
 
     k = argmax(map(+, x1, reverse(x2))) - 1
 
@@ -82,9 +146,5 @@ function readwv(filename)
 
     return (W, N, w, v)
 end
-
-precompile(readwv, (String,))
-precompile(dncknapsack, (Int64, UnitRange{Int64}, Vector{Int64}, Vector{Int64}))
-precompile(ilpknapsack, (Int64, Vector{Int64}, Vector{Int64}))
 
 end
